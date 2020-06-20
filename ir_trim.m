a53d40bead42e2dfe79c07f44ad271570aafa5e6
ziyun-liu <ppa.ziyun.liu@gmail.com>
2020-06-20 22:23:32 +0800

    Update ir_trim.m

--- ir_trim.m
+++ ir_trim.m
@@ -8,6 +8,8 @@ function [ IR_trim , peak_idx ] = ir_trim( IR, n_trim, pre_delay)
 
 if (peak_idx-1 + n_trim-pre_delay) > length(IR)
     IR_trim = [IR(peak_idx-pre_delay+1:end);IR(1:(peak_idx + n_trim-pre_delay)-length(IR))];
+elseif peak_idx-pre_delay<1
+    IR_trim = [IR(length(IR)+peak_idx-pre_delay : end); IR( 1 : peak_idx-1 + n_trim-pre_delay)];
 else
     IR_trim = IR( peak_idx-pre_delay : peak_idx-1 + n_trim-pre_delay);
 end
